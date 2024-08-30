722012162002c5f905e31948e2648004953a8a5d
wifv <abcogenshin@gmail.com>
2024-08-30 23:41:38 +0500

    landing

--- src/assets/pages/Landing.jsx
+++ src/assets/pages/Landing.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import '../css/landing.css'
+import huh from '../images/huh.webp'
+import Products from './Products';
 
 const landing = () => {
   return (
@@ -10,8 +12,53 @@ const landing = () => {
         slidesPerView={1}
         loop={true}
       >
-        <SwiperSlide>bomba</SwiperSlide>
+        <SwiperSlide style={{backgroundImage: `url(${huh})`}} className='big-swiper'></SwiperSlide>
+        <SwiperSlide style={{backgroundImage: `url(${huh})`}} className='big-swiper'></SwiperSlide>
+        <SwiperSlide style={{backgroundImage: `url(${huh})`}} className='big-swiper'></SwiperSlide>
       </Swiper>
+
+      <section className="categories-section">
+        <h1>Categories</h1>
+
+        <div className="big-categories">
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+          <div className="category-item">
+            <img src={huh} className='category-img'/>
+            <h3>atratikalomkpch</h3>
+          </div>
+        </div>
+
+        <button type='button' className='see-categories'>See All</button>
+      </section>
+
+      <Products />
     </div>
   )
 }
---
+++ src/assets/pages/Products.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import huh from '../images/huh.webp'
+import '../css/products.css'
+
+const Products = () => {
+  return (
+    <section>
+      <h1>Products</h1>
+      
+      <div className="products">
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+        <div className="product">
+          <img src={huh} alt="product-image" className='product-image'/>
+          <h2 className="product-name">qalesan xona</h2>
+          <h1 className='product-price'>10000 bitcoin</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Products
